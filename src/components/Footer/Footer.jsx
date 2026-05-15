d45a97849c14e28586d80693ab79d8e4c47b72e0
MartinTigani <martin.sebastian.999.09@gmail.com>
2026-05-15 18:36:09 -0300

    primer comiteo

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import "./Footer.css";
+
+export const Footer = () => {
+    return(
+        <footer>
+            <p>En caso de duda, contáctanos</p>
+            <nav>
+                <ul className="nav-list">
+                    <li><a href="www.whatsapp.com">Whatsapp</a></li>
+                    <li><a href="www.instagram.com">Instagram</a></li>
+                    <li><a href="www.facebook.com">Facebook</a></li>
+                </ul>
+            </nav>
+        </footer>
+    )
+    
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+import logo from "../../assets/react.svg"
+import "./Header.css"
+import { Nav } from "../Nav/Nav"
+
+
+export const Header = () => {
+    return <header>
+        <div className="logo.container">
+            <Link to={'/'}>
+                <img src={logo} alt="Logo" />
+                <span>Reactiva</span>
+            </Link>
+        </div>
+        <Nav />
+    </header>
+}
